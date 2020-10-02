1204ecc32e4be3511f4549c42940e43af3ff5e3a
Stefan <stefanwk@gmail.com>
2020-10-02 14:17:18 -0300

    Map with current location

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { Text, View, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Map } from './Map';
+import AsyncStorage from '@react-native-community/async-storage';
+import Map from './Map';
 
 function HomeScreen() {
   return (
@@ -13,24 +14,57 @@ function HomeScreen() {
   );
 }
 
+const saveData = async (level) => {
+  try {
+    await AsyncStorage.setItem('@level_key', level.toString())
+    alert('Data successfully saved '+level.toString())
+    console.log('Data successfully saved ')
 
+  } catch (e) {
+    console.log('Error saving level value', e);
+  }
+}
+
+const readData = async () => {
+  try {
+    const userLevel = await AsyncStorage.getItem('@level_key')
+    alert('Successfully fetched the data from storage: '+userLevel)
+    console.log('Successfully fetched the data from storage: '+userLevel)
+    if (userLevel !== null) {
+      console.log('not null '+Promise.resolve(userLevel))
+      return Promise.resolve(userLevel)
+    }
+  } catch (e) {
+    alert('Failed to fetch the data from storage')
+    console.log(e)
+  }
+}
 
 function SettingsScreen() {
-  const [level, setLevel] = useState(0);
+  
+  //const data =  readData();
+  //console.log(data);
+  //let userLevel = data !== 'NaN' ? data: 0;
+  //      <Button title={"Press "+userLevel} onPress={() => {/*saveData(Number.parseInt(userLevel,10)+1),*/ console.log('Ul: '+userLevel)}} />
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings</Text>
-      <Button title={"Press "+level} onPress={() => {setLevel(level+1); console.log(level)}} />
     </View>
   );
 }
 
+
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Tab.Navigator
+        initialRouteName = 'Mapa'
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
@@ -50,6 +84,8 @@ export default function App() {
         tabBarOptions={{
           activeTintColor: 'skyblue',
           inactiveTintColor: 'gray',
+          style: { height: 75},
+          tabStyle: { marginTop: 5, marginBottom: -8}
         }}
       >
         <Tab.Screen name="Clave" component={HomeScreen} />
--- Map.js
+++ Map.js
@@ -1,10 +1,144 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import {
+  Alert,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import MapView from 'react-native-maps'
 
-export function Map() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Mapa</Text>
-    </View>
-  );
+const LATITUDE_DELTA = 0.1;
+const LONGITUDE_DELTA = 0.1;
+
+const initialRegion = {
+  latitude: -39.29,
+  longitude: -72.15,
+  latitudeDelta: 10,
+  longitudeDelta: 10,
 }
+const markers = [];
+
+class MyMapView extends React.Component {
+
+  map = null;
+
+  state = {
+    region: initialRegion,
+    ready: true,
+    filteredMarkers: []
+  };
+
+  setRegion(region) {
+    console.log(region);
+    if(this.state.ready) {
+      setTimeout(() => this.map.animateToRegion(region,2000), 10);
+    }
+    //this.setState({ region });
+  }
+
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
+
+  getCurrentPosition = () => {
+    try {
+      this.setRegion(this.state.region);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const region = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          };
+          this.setRegion(region);
+        },
+        (error) => {
+          //TODO: better design
+          switch (error.code) {
+            case 1:
+              if (Platform.OS === "ios") {
+                Alert.alert("", "Para ubicar tu locación habilita permiso para la aplicación en Ajustes - Privacidad - Localización");
+              } else {
+                Alert.alert("", "Para ubicar tu locación habilita permiso para la aplicación en Ajustes - Apps - ExampleApp - Localización");
+              }
+              break;
+            default:
+              Alert.alert("", "Error al detectar tu locación");
+              console.log(error);
+          }
+        }
+      );
+    } catch(e) {
+      alert(e.message || "");
+    }
+  };
+
+  onMapReady = (e) => {
+    if(!this.state.ready) {
+      this.setState({ready: true});
+    }
+  };
+
+  onRegionChange = (region) => {
+    console.log('onRegionChange', region);
+  };
+
+  onRegionChangeComplete = (region) => {
+    console.log('onRegionChangeComplete', region);
+  };
+
+  render() {
+
+    const { region } = this.state;
+    const { children, renderMarker, markers } = this.props;
+    console.log(this.props);
+
+    return (
+      <View style={styles.container}>
+        
+        <MapView
+          showsUserLocation
+          userLocationAnnotationTitle={'Ubicación actual'}
+
+          ref={ map => { this.map = map }}
+          //data={markers}
+          initialRegion={initialRegion}
+          renderMarker={renderMarker}
+          onMapReady={this.onMapReady}
+          showsMyLocationButton={true}
+          //onRegionChange={this.onRegionChange}
+          //onRegionChangeComplete={this.onRegionChangeComplete}
+          style={StyleSheet.absoluteFill}
+          textStyle={{ color: '#bc8b00' }}
+          containerStyle={{backgroundColor: 'white', borderColor: '#BC8B00', }}>
+        </MapView>
+        <TouchableOpacity style={styles.button} activeOpacity = "0.9" onPress={this.getCurrentPosition}>
+          <FontAwesome name="location-arrow" size={20} color="white"/>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end'
+  },
+  button: {
+   alignItems:'center',
+   justifyContent:'center',
+   width:50,
+   height:50,
+   backgroundColor:'skyblue',
+   borderRadius:50,
+   marginRight: 10,
+   marginBottom: 30
+ }
+});
+
+export default MyMapView;
